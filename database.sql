f5220cc6667a4fd813f6ed0fd47546d2058f3fcc
oliluoma <n3luol00@students.oamk.fi>
2024-12-10 10:16:13 +0200

    adding parameters

--- database.sql
+++ database.sql
@@ -1,14 +1,14 @@
 -- Active: 1731535021677@@127.0.0.1@5432@postgres
 CREATE TABLE genre (
-    genre_id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    genre_name VARCHAR(255) NOT NULL
+    genre_name TEXT PRIMARY KEY,
+    genre_id int
 );
 CREATE TABLE movie (  
-    movie_id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    movie_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title VARCHAR(255) NOT NULL,
     year int,
-    genre_id int,
-    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+    genre_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (genre_name) REFERENCES genre(genre_name)
   
 );
 
@@ -40,3 +40,19 @@ CREATE TABLE favorite (
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
   
 );
+
+INSERT INTO genre (genre_name) VALUES 
+('drama'),('comedy'),('scifi'),('fantasy'),('action'),('triller');
+
+INSERT INTO movie (title, year, genre_name) VALUES 
+('Inception', 2010, 'action'),
+('The Terminator', 1984, 'action'),
+('Tropic Thunder', 2008, 'comedy'),
+('Borat', 2006, 'comedy'),
+('Interstellar', 2014, 'drama'),
+('Joker', 2019, 'drama');
+
+INSERT INTO customer (username, name, password, birth_year) VALUES
+('reimarii', 'Reima Riihimäki', 'qwerty123', 1986),
+('lizzy', 'Lisa Simpson', 'abcdef', 1991 ),
+('boss', 'Ben Bossy', 'salasana', 1981 )
